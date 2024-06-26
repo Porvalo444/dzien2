4792360a4112606ac65fbe69bdcb7750020762d8
Porvalo444 <bartosz.zarzeczniak@zse.krakow.pl>
2024-06-26 19:22:40 +0200

    updated colors and placement

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h2 class="text-blue-600">Wpisy na bloga:</h2>
-        <button @click="pobierzWpisy">refresh</button>
-        <div v-for="wpisypis in wpisy">
-            <p>{{ wpis }}</p>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class ="bg-blue-600 rounded-sm text-white p-4">refresh</button>
         </div>
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+        <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
+        <button class ="bg-blue-600 rounded-sm text-white p-4" @click="dodajWpisy">dodaj</button>
+    </div>
     </div>
 </template>
 
